b64ecb5cf19302c300d543648fa211a9459e4293
Sam <55955188+SamGrig96@users.noreply.github.com>
2021-05-05 18:26:27 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,22 +9,18 @@ const App = () => {
   const [age, setAge] = useState('');
   const [search, setSearch] = useState('');
   const [myArray, updateMyArray] = useState(JSON.parse(localStorage.getItem('mytime')) || []);
-  const [count, setCount] = useState(1);
   const [filteredPerson, setFilteredPerson] = useState([]);
 
 
   const onClick = () => {
-
-setCount(count=>count+1)
- 
     let myData = {
       name: name,
       surname: surname,
       age: age,
-      count: count
+
     };
 
-    updateMyArray([...myArray,myData])
+    updateMyArray([...myArray, myData])
     setsurName('');
     setName('')
     setAge('')
@@ -34,14 +30,14 @@ setCount(count=>count+1)
 
 
   useEffect(() => {
-   
-   if(myArray.length===0){
-  localStorage.clear()
-   } else{
-    localStorage.setItem("mytime", JSON.stringify(myArray))
-   }
 
-   
+    if (myArray.length === 0) {
+      localStorage.clear()
+    } else {
+      localStorage.setItem("mytime", JSON.stringify(myArray))
+    }
+
+
     setFilteredPerson(
       myArray.filter((person) =>
         person.name.toLowerCase().includes(search.toLocaleLowerCase()) ||
@@ -54,14 +50,12 @@ setCount(count=>count+1)
 
   return [
     <div>
-
-
       <div className='App' >
         <input type='text' onChange={(e) => { setName(e.target.value) }} value={name} placeholder='Name' />
         <input type='text' onChange={(e) => { setsurName(e.target.value) }} value={surname} placeholder='Surname' />
         <input type='number' onChange={(e) => { setAge(e.target.value) }} value={age} placeholder='Age' />
-        <input type="button" className='btn-success' onClick={onClick} value="Add" disabled={name.length===0} />
-        <input type="text" placeholder="Search" value={search} onChange={(e) => setSearch(e.target.value)}  />
+        <input type="button" className='btn-success' onClick={onClick} value="Add" disabled={name.length === 0} />
+        <input type="text" placeholder="Search" value={search} onChange={(e) => setSearch(e.target.value)} />
       </div>
       <div>
         <table className='table-one table table-dark ' style={{ width: '55%', margin: 'auto' }}>
@@ -75,7 +69,7 @@ setCount(count=>count+1)
           </thead>
         </table>
         {filteredPerson.map((persons, index) => (
-          <Person key={index} {...persons} />))}
+          <Person key={index} {...persons} index={index} />))}
       </div>
 
     </div>
@@ -85,14 +79,14 @@ setCount(count=>count+1)
 
 
 const Person = (props) => {
-  const { name, surname, age, count } = props;
+  const { name, surname, age, count, index } = props;
 
   return (
     <div>
       <table className='tabel-two table table-striped table-dark' style={{ width: '55%', margin: 'auto', marginTop: '15px' }}>
         <tbody>
           <tr>
-            <td>{count}</td>
+            <td>{props.index + 1}</td>
             <td>{name}</td>
             <td>{surname}</td>
             <td>{age}</td>
